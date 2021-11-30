8c9546431ca0fe356ca202daea35dd70bdf26a47
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-30 23:40:24 +0300

    CurrentConditionsDisplay added

---
+++ src/main/java/org/example/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,32 @@
+package org.example.observer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.subject.Subject;
+
+@Getter
+@AllArgsConstructor
+public class CurrentConditionsDisplay implements Observer, DisplayElement{
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
--- src/main/java/org/example/subject/WeatherData.java
+++ src/main/java/org/example/subject/WeatherData.java
@@ -41,6 +41,7 @@ public class WeatherData implements Subject {
         this.temperature = temperature;
         this.humidity = humidity;
         this.pressure = pressure;
+        measurementsChanged();
     }
 
 }
