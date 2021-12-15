5e7feb5aee6c1dff4165c4718527d39d18455733
Rishat Suleymanov <spk_irbis@mail.ru>
2021-12-15 20:07:18 +0300

    Init decorator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,8 @@
 package org.example;
 
-import org.example.observer.CurrentConditionsDisplay;
-import org.example.subject.WeatherData;
-
 public class App {
     public static void main(String[] args) {
-        WeatherData weatherData = new WeatherData();
 
-        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
-
-        weatherData.setMeasurements(80, 65, 30.4f);
-        weatherData.setMeasurements(82, 70, 29.2f);
-        weatherData.setMeasurements(78, 90, 29.2f);
+        System.out.println("Decorator");
     }
 }
