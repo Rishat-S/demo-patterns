950d2ff23eb8942c7a8592f2fed56404dd2456b2
Rishat Suleymanov <spk_irbis@mail.ru>
2021-12-17 21:50:42 +0300

    Initial fabrica

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,7 @@
 package org.example;
 
-import org.example.entity.beverage.Beverage;
-import org.example.entity.beverage.Espresso;
-import org.example.entity.condiment.Milk;
-import org.example.entity.condiment.Mocha;
-import org.example.entity.condiment.Whip;
-
 public class App {
     public static void main(String[] args) {
-        Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
-
-        Beverage beverage1 = new Milk(new Whip(new Mocha(new Espresso())));
-        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
-
-
+        System.out.println("fabric");
     }
 }
