a758ebd04e4affa15446028118b047bbfc592729
Rishat Suleymanov <spk_irbis@mail.ru>
2022-01-30 16:59:31 +0300

    Command to on light created

---
+++ src/main/java/org/example/device/Light.java
@@ -0,0 +1,7 @@
+package org.example.device;
+
+public class Light {
+    public void on() {
+        System.out.println("Light on.");
+    }
+}
---
+++ src/main/java/org/example/remote/SimpleRemoteControl.java
@@ -0,0 +1,19 @@
+package org.example.remote;
+
+import org.example.command.Command;
+
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl() {
+    }
+
+    public void setCommand(Command command) {
+        slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+
+}
